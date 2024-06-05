6d8028a6d3c59688d186fb951579302df586c8cf
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-06-05 16:49:07 +0530

    3.tablets of 2,3 && 4

---
+++ Loops/Table _of _2 _3  _4.py
@@ -0,0 +1,31 @@
+def printTables():
+    
+    for i in range(1,11):
+        print(2 * i, end=" ")
+    print("")
+    for i in range(1,11):
+        print(3 * i, end=" ")
+    print("")
+    for i in range(1,11):
+        print(4 * i, end=" ")
+    print("")
+    
+
+
+# Problem Statement:
+
+# Generate and print the multiplication tables(only 10) for the numbers 2, 3, and 4  using loops. Each table should be space-separated integers, and each table should start on a new line.
+
+# The tables should be printed up to the 10th multiple.
+
+# Input Format:
+# No specific input is required as the tables are predefined for 2, 3, and 4.
+# Output Format:
+
+# Print the multiplication tables for 2, 3, and 4, each up to the 10th multiple. Each table should be space-separated integers, and each table should start on a new line.
+
+# Output :
+
+# 2 4 6 8 10 12 14 16 18 20
+# 3 6 9 12 15 18 21 24 27 30
+# 4 8 12 16 20 24 28 32 36 40
